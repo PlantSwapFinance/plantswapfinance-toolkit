0d8a46a7381ba890342bce3f02c203ce0e460e6c
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2026-07-30 11:32:34 -0400

    fix(storybook): migrate config and stories for v10
    - switch framework to @storybook/react-vite with options object
    - drop @storybook/addon-essentials and @storybook/addon-actions
      (now built-in in v10 and v9 of these packages is an empty shell)
    - enable react-docgen-typescript for TypeScript stories
    - replace @storybook/react/types-6-0 imports with @storybook/react
    - update CLI scripts from start-storybook/build-storybook to storybook dev/build
    - add storybook CLI and vite as dev deps

--- packages/plantswap-uikit/src/components/TabMenu/index.stories.tsx
+++ packages/plantswap-uikit/src/components/TabMenu/index.stories.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 /* eslint-disable import/no-unresolved */
-import { Meta } from "@storybook/react/types-6-0";
+import { Meta } from "@storybook/react";
 import TabMenu from "./TabMenu";
 import Tab from "./Tab";
 
